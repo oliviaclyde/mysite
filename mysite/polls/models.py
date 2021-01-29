f111b6030d892fff97b6b1d6a6371aac0661774d
oliviaclyde <olivialclyde@gmail.com>
2021-01-29 15:44:41 -0700

    wrote inital tests and then wrote models to pass the tests, ran migrations

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+# from django.utils.timezone import now
 
 # Create your models here.
+# class Question(models.Model):
+#     question_text = models.CharField(default='', max_length=200)
+#     pub_date = models.DateTimeField('date published', null=True)
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE, default='')
+#     choice_text = models.CharField(default='', max_length=200)
+#     votes = models.IntegerField(default=0)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published', blank=True, null=True)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from . views import index
+from . models import Question, Choice
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -13,4 +14,20 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('Hello, world. You\'re at the polls index.', html)
 
-        
+
+    def test_saving_and_retrieving_items(self):
+        first_question = Question()
+        first_question.question_text = 'What is your favorite color?'
+        first_question.save()
+
+        second_question = Question()
+        second_question.question_text = 'What is the best day of the week?'
+        second_question.save()
+
+        saved_items = Question.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_question = saved_items[0]
+        second_saved_question = saved_items[1]
+        self.assertEqual(first_saved_question.question_text, 'What is your favorite color?')
+        self.assertEqual(second_saved_question.question_text, 'What is the best day of the week?')
